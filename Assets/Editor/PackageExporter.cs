ccdea091b18c97f944a11f27dba13d9c03795406
kyubuns <kyubuns@gmail.com>
2017-04-27 12:15:23 +0900

    package exporter

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class PackageExporter
+{
+	[MenuItem("Package/Export Unity Package")]
+	public static void ExportUnityPackage()
+	{
+		var libraryDirectories = new[]
+		{
+			"Assets/Plugins/UniMail"
+		};
+
+		var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "UniMail.unitypackage");
+		Debug.LogFormat("Export package to {0}", outputFilePath);
+		AssetDatabase.ExportPackage(libraryDirectories, outputFilePath, ExportPackageOptions.Recurse);
+		Debug.LogFormat("Success");
+	}
+}
