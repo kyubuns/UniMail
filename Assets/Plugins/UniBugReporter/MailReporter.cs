cab645ec492daf68aa0112423e93b98aa1f37713
kyubuns <kyubuns@gmail.com>
2018-12-30 22:29:02 +0900

    feat: MailReporter

---
+++ Assets/Plugins/UniBugReporter/MailReporter.cs
@@ -0,0 +1,32 @@
+﻿using UniMail;
+using UnityEngine;
+
+namespace UniBugReporter
+{
+    public class MailReporter : IReporter
+    {
+        private const string MailTo = "dummy@example.com";
+        private const string Subject = "BugReport";
+
+        public bool IsReportTarget(string message, string stackTrace, LogType type)
+        {
+            return type == LogType.Assert || type == LogType.Exception;
+        }
+
+        public void Report(string message, string stackTrace, LogType type, string screenshotFilePath)
+        {
+            var body = "{ERROR_MESSAGE}\n---\n{STACK_TRACE}";
+            body = body.Replace("{ERROR_MESSAGE}", message);
+            body = body.Replace("{STACK_TRACE}", stackTrace);
+
+            if (string.IsNullOrEmpty(screenshotFilePath))
+            {
+                Mail.Send(MailTo, Subject, body);
+            }
+            else
+            {
+                Mail.SendWithImage(MailTo, Subject, body, screenshotFilePath);
+            }
+        }
+    }
+}
